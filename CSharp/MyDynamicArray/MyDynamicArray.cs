2d549ca18dd45b3e36e33c6909c4d435a5fc4648
sin yo han <a01050353566@gmail.com>
2023-03-25 18:57:38 +0900

    삽입, 삭제, 탐색 알고리즘들
    집에가서 복습해야 된다

--- CSharp/MyDynamicArray/MyDynamicArray.cs
+++ CSharp/MyDynamicArray/MyDynamicArray.cs
@@ -53,7 +53,7 @@ namespace Collections
 
         //***삽입 알고리즘***
         //일반적인 경우에는 O(1)
-        //Capacity(배열의 크기)가 모자랄 때는 O(N)
+        //Capacity(배열의 크기)가 모자랄 때는  더 큰 배열을 만들고 데이터를 전부 복제해야허가 떼문에O(N)
         public void Add(object item)
         {
             //여유 공간이 부족하면 더 큰 크기의 배열을 만들고 기존 데이터 복제후 기존 배열을 날림
@@ -77,7 +77,7 @@ namespace Collections
         }
 
         //***탐색 알고리즘***
-        //O(N)
+        //인텍스 접근하기 떼문에O(1),만약 특정값이나 조건에 해당되는 아이템을 찾으려면 O(N)
         public bool Contains(object item)
         {
             for (int i = 0; i < _count; i++)
@@ -112,7 +112,7 @@ namespace Collections
         }
 
         //***삭제 알고리즘***
-        //O(N)
+        //삭제하고 싶은 인덱스 다음거부터 끝까지 전부 한칸씩 앞으로 당기기 떼문에 O(N)
         public bool RemoveAt(int index)
         {
             if (index < 0 || index >= _count)
@@ -131,6 +131,17 @@ namespace Collections
         {
             return RemoveAt(FindIndex(item));
         }
+
+        //LinkedList : 자료를 담는 기본단위인 노드(Node) 단위로 자료를 구성하고, 노드(Note)끼리 연결시킴. 노드(Note)들은 힙영역에 랜덤하게 분포됨
+        //삽입 : 맨앞/뒤에 삽입할 경우 O(1), 특정 노드 앞/뒤에 삽입할 경우 O(1)
+        //탐색 : O(N)
+        //삭제 : O(1)
+        //Hashtable : 추가하고자 하는 값에 대해서 Hash 함수를 통해 고유한 Key 값을 산출하고, 그 Key값으로 인덱스에 접근하는 형태의 테이블
+        //헤시키의 충돌에 해결방법? : LinkedList Buket : 해시 테이블에 value를 개별 LinkedList로 만들어서 충돌이 일어나는 값들을 모두 개별 노드로 저장하는 방법, Open Addressing : 해시키를 다시 구한다
+        //삽입 : 거의 O(1), 충돌이 일어날 경우 LinkedList : O(1), OpenAddressing : 배열을 늘려야한다면 그때는 O(N)
+        //탐색 : 해시함수를 통해 인덱스접근하기 때문에 거의 O(1)
+        //삭제 : 해시함수를 통해 인덱스접근하기 때문에 거의 O(1)
+
 
     }
 }
--- CSharp/MyDynamicArray/Program.cs
+++ CSharp/MyDynamicArray/Program.cs
@@ -1,10 +1,18 @@
 ﻿using Collections;
+using MyDynamicArray;
 using System.Collections;
+using System.Net.Sockets;
 using System.Security.Cryptography.X509Certificates;
 //자료 구조
 //자료구조에서 공통적으로 구현하는 요소들은
 //탐색, 삽입, 삭제 알고리즘
 //현재 자료 갯수
+
+
+//Enumerator : 열거된 자료를 순회할 수 있는 타입/ 특정로직을 순서대로 순회할 수 있는 타입
+//Current : 현재 참조하고 있는 자료/로직의 상태(스텝)
+//MoveNext() : 다음 단계로 이동
+//Reset() : Current를 end(default)로 초기화
 
 #region 동적 배열
 MyDynamicArray myDynamicArray = new MyDynamicArray();
@@ -36,7 +44,7 @@ for (int i = 0; i < myDynamicArray.Count; i++)
 // 이타입에 다른 객체를 참조시킬 때 힙영역에 System.Object 타입의 객체가 할당됨
 int a = 5;
 object obj = a;// System.Object 타입 참조 변수
-obj = a;// a를 SYStem.Object 타입으로 변환한 객체를 힙영역에 할당하고 참조를 변환함 (Boxing) : 참조하는 변수 =>  객체(System.object)를 만들고 그(int= a) 주소를 참조함 일반적인 대입연산보다 얃 20배 느림
+obj = a;// a를 System.Object 타입으로 변환한 객체를 힙영역에 할당하고 참조를 변환함 (Boxing) : 참조하는 변수 =>  힙영역에 객체(System.object)를 만들고 그(int= a) 주소를 만든다. 일반적인 대입연산보다 얃 20배 느림
 obj = myDynamicArray;
 MyDynamicArray da2 = (MyDynamicArray)obj; // Object 타입을 하위 타입으로 변환 (Unboxing)
 
@@ -61,7 +69,7 @@ arrayList.Add("안녕");
 //--------------------------------------------
 
 MyDynamicArray<double> doubleArray = new MyDynamicArray<double>();
-
+//<T> : 타입이 정해지지 않은 클래스
 doubleArray.Add(1.0);
 doubleArray.Add(5.0);
 doubleArray.Add(6.0);
@@ -94,6 +102,64 @@ doubleList.FindIndex(x => x == 3.0);
 foreach (double item in doubleList)
 { 
 
+}
+
+#endregion
+
+#region 연결리스트
+//내가 만든 연결리스트
+//--------------------------------
+MyLinkedList<int> intLinkedList = new MyLinkedList<int>();
+intLinkedList.AddLast(2);
+intLinkedList.AddLast(3);
+intLinkedList.AddLast(5);
+MyLinkedListNode<int> dummy = intLinkedList.FindLast(5);
+intLinkedList.AddAfter(dummy, 6);
+
+foreach (int value in intLinkedList)
+{
+    Console.WriteLine($"내 연결리스트 순화중... {value}");
+}
+
+// C# System.Collecions.Generic.LinkedList
+//--------------------------------
+LinkedList<float>floats = new LinkedList<float>();
+floatLinkedList.AddFirst(3);
+LinkedListNode<float>?dummy2 = floatLinkedList.FindLast(3);
+floatLinkedList.AddAfter(dummy2, 5);
+
+
+#region generic Dictionary
+//내가 만든 generic Dictionary
+//--------------------------------
+MyDictionary<string, int> scores = new MyDictionary<string, int>();
+scores.Add("철수", 80);
+scores.Add("영희", 70);
+scores.Remove("영희");
+Console.WriteLine(scores["철수"]);
+
+
+//C# System.Collections.Generic.Dictioanry
+//-------------------------------
+Dictionary<string, char> grades = new Dictionary<string, char>();
+grades.Add("철수", 'A');
+grades.Add("영희", 'C');
+grades.Remove("영희");
+
+foreach (KeyValuePair<string, char> grade in grades)
+{
+    Console.WriteLine($"{grade.Key}의 등급 : {grade.Value}");
+}
+
+
+foreach (string key in grades.Keys)
+{
+    Console.WriteLine($"학급생{key}");
+}
+
+foreach (char value in grades.Values)
+{
+    Console.WriteLine($"등급표{value}");
 }
 
 #endregion
