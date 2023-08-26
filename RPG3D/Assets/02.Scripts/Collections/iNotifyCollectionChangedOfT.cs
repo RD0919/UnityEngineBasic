ebb3a869b7c472e0ae6ae110b0fd4b7fbe1c2e30
RD <a01050353566@gmail.com>
2023-08-26 18:41:50 +0900

    3D RPG

---
+++ RPG3D/Assets/02.Scripts/Collections/iNotifyCollectionChangedOfT.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public interface INotifyCollectionChangedOfT<T>
+{
+    event Action<int, T> ItemAdded;
+    event Action<int, T> ItemRemoved;
+    event Action<int, T> ItemChanged;
+    event Action CollectionChanged;
+}
